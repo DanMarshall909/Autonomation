dcaedc41e5a6d844609c1d12842f0ee90cac0146
Dan Marshall <danmarshall909@gmail.com>
2024-06-16 13:51:18 +1000

    Add trivial case

---
+++ Autonomation.Core/PalindromeGenerator.cs
@@ -0,0 +1,9 @@
+namespace Autonomation.Core;
+
+public class PalindromeGenerator
+{
+    public List<string> Generate(string s)
+    {
+        return [s];
+    }
+}
---
+++ Autonomation.Test/PalindromeGeneratorTests.cs
@@ -0,0 +1,16 @@
+﻿using Autonomation.Core;
+using FluentAssertions;
+
+namespace Autonomation.Test;
+
+public class PalindromeGeneratorTests
+{
+    [Fact(DisplayName = "Generate palindrome of length 1")]
+    public void generate_palindrome_of_length_1()
+    {
+        var palindromeGenerator = new PalindromeGenerator();
+        
+        var result = palindromeGenerator.Generate("a");
+        result.Should().BeEquivalentTo(new List<string>{"a"});
+    }
+}
