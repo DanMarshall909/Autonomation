3f34dcaf4e1944da600d457dc1c1b1a37ec02c6d
Dan Marshall <danmarshall909@gmail.com>
2024-06-16 18:21:28 +1000

    Update Console app

---
+++ Autonomation.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Autonomation.Core;
+
+var input = "";
+while (true)
+{
+    if (args.Length == 0)
+    {
+        while (input == "")
+        {
+            Console.WriteLine("Enter a string to partition into palindromes (q quits):");
+            input = Console.ReadLine();
+        }
+    }
+    else
+    {
+        input = args[0];
+    }
+
+    if (input == "q")
+    {
+        Console.WriteLine("Quitting...");
+        break;
+    }
+
+    var partitions = PalindromeService.CommaSeparatedPartitions(input);
+    Console.WriteLine($"The palindromic partitions of {input} are:");
+    foreach (var partition in partitions)
+    {
+        Console.WriteLine(partition);
+    }
+
+    input = "";
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Ariva derci!");
